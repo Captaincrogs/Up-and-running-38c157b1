78729c7b7dfbcd80dfad0d3e6b3b65862bc0ed68
Captaincrogs <54841728+Captaincrogs@users.noreply.github.com>
2020-03-15 18:11:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-echo phpinfo();
+    <?php
+
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "netland";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=netland", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connectie met database is succesvol";
+    }
+catch(PDOException $e)
+    {
+    echo "kan geen connectie met database maken!: " . $e->getMessage();
+    }
+
 ?>
 
 
 </body>
+
 </html>
